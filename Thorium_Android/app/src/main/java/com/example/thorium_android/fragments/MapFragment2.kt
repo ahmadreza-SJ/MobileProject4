f8995cfdac651fe179c18fdb6c840443a8511b1d
mahsaghaderan99 <mahsa.ghaderan@gmail.com>
2021-07-11 15:10:22 +0430

    Fix map crash

--- Thorium_Android/app/src/main/java/com/example/thorium_android/fragments/MapFragment2.kt
+++ Thorium_Android/app/src/main/java/com/example/thorium_android/fragments/MapFragment2.kt
@@ -5,40 +5,52 @@ import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.Gravity
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.thorium_android.R
 import com.example.thorium_android.utils.MapHelpers
 import com.example.thorium_android.view_models.LocationViewModel
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.Marker
+
 
 class MapFragment2 : Fragment(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mapView : MapView
     private lateinit var locationViewModel: LocationViewModel
     private var activeMarkers = mutableListOf<Marker>()
     var color_map = mutableMapOf<Int, Int>()
     private val filters =
-        arrayOf("CID", "LAC/TAC", "Cell Type", "MCC", "MNC","PLMN", "ARFCN")
+        arrayOf("CID", "LAC/TAC", "Cell Type", "MCC", "MNC", "PLMN", "ARFCN")
     var color_method = "CID"
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map2, container, false)
+        val theview =  inflater.inflate(R.layout.fragment_map2, container, false)
+        return theview
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        println("on view created..........")
+        mapView = view?.findViewById(R.id.map) as MapView
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume();
+        mapView.getMapAsync(this)
         locationViewModel = ViewModelProvider(this).get(LocationViewModel::class.java)
         val spinner: Spinner = requireView().findViewById(R.id.spinner)
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, filters)
@@ -80,12 +92,14 @@ class MapFragment2 : Fragment(), OnMapReadyCallback {
 
     @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
+        println("on map ready..........")
         mMap = googleMap
         mMap.isMyLocationEnabled = true
         mMap.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
             override fun getInfoWindow(arg0: Marker): View? {
                 return null
             }
+
             override fun getInfoContents(marker: Marker): View {
                 val info = LinearLayout(requireContext().applicationContext)
                 info.orientation = LinearLayout.VERTICAL
